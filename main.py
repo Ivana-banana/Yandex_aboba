17e673f32ece6075ff021471ef9327c3c9d8c569
Ivan <Student@student.com>
2024-03-01 17:56:37 +0300

    Случайные окружности

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 
 
+class Fon():
+    def __init__(self, file):
+        uic.loadUi(file, self)
+
+
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        Fon("UI.ui")
         self.initUI()
 
     def initUI(self):
         self.do_paint = False
-        width = self.size().width()
-        height = self.size().height()
-        self.x = random.randint(40, width - 40)
-        self.y = random.randint(30, height - 30),
-        self.r = random.randint(10, 40)
+        self.width = self.size().width()
+        self.height = self.size().height()
         self.pushButton.clicked.connect(self.paint)
 
     def paintEvent(self, event):
@@ -35,8 +37,13 @@ class Example(QMainWindow):
         self.update()
 
     def draw_flag(self, qp):
-        qp.drawEllipse(self.x, self.y,
-                       self.r, self.r)
+        for el in range(5):
+            self.x = random.randint(40, self.width - 40)
+            self.y = random.randint(30, self.height - 30)
+            self.r = random.randint(10, 40)
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+            qp.drawEllipse(self.x, self.y,
+                           self.r, self.r)
 
 
 if __name__ == '__main__':
