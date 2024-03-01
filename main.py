9bb72ee70ec0ddef326b21eb8172f8464cc1c742
Ivan <Student@student.com>
2024-03-01 21:52:39 +0300

    Эспрессо

--- main.py
+++ main.py
@@ -1,54 +1,36 @@
-import random
+import sqlite3
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class Fon():
-    def __init__(self, file):
-        uic.loadUi(file, self)
-
-
-class Example(QMainWindow):
+class DBSample(QMainWindow):
     def __init__(self):
         super().__init__()
-        super().__init__()
-        Fon("UI.ui")
-        self.initUI()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.sqlite")
+        self.select_data()
 
-    def initUI(self):
-        self.do_paint = False
-        self.width = self.size().width()
-        self.height = self.size().height()
-        self.pushButton.clicked.connect(self.paint)
+    def select_data(self):
+        res = self.connection.cursor().execute("SELECT * FROM coffee").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_flag(qp)
-            qp.end()
-        self.do_paint = False
-
-    def paint(self):
-        self.do_paint = True
-        self.update()
-
-    def draw_flag(self, qp):
-        for el in range(5):
-            self.x = random.randint(40, self.width - 40)
-            self.y = random.randint(30, self.height - 30)
-            self.r = random.randint(10, 40)
-            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-            qp.drawEllipse(self.x, self.y,
-                           self.r, self.r)
+    def closeEvent(self, event):
+        self.connection.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = DBSample()
     ex.show()
     sys.exit(app.exec())
 
